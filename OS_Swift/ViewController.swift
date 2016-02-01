b4e3d947f7b0d358563abddabba4212f4d42fd72
Bruce Chen <cqli@applematomacbook-pro.local>
2016-02-01 17:02:06 +0800

    Initial Commit

---
+++ OS_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OS_Swift
+//
+//  Created by li, Chaoqing on 2/1/16.
+//  Copyright © 2016 Bruce Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
